651c65276fb2746e9f676c426c4b180336985e4a
riothung <mario.thung@gmail.com>
2025-02-11 19:47:28 +0800

    add backend

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+require './partials/header.php';
+
+?>
+
+<div class="d-sm-flex align-items-center mb-4" style="display: flex;">
+    <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require './partials/footer.php'; ?>
