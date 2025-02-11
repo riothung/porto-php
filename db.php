ca4fdc8e79b79f801f9eec87ad11dc8e5cf9b111
riothung <mario.thung@gmail.com>
2025-02-11 17:45:00 +0800

    feat: connect to database

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ function OpenCon()
     $dbhost = "localhost";
     $dbuser = "root";
     $dbpass = "";
-    $db = "";
+    $db = "uas_aldi";
 
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
 
