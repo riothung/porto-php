0d7a7ea24f11d5c8bb9b7846935facc693b05379
riothung <mario.thung@gmail.com>
2025-02-12 09:14:56 +0800

    tes webhooks niagahoster

--- dashboard.php
+++ dashboard.php
@@ -6,23 +6,23 @@ require './partials/header.php';
 
 
 
-  <!-- Begin Page Content -->
-  <div class="container-fluid">
-        <!-- Welcome Message Section -->
-        <div class="row justify-content-center">
-          <div class="col-lg-6 col-md-8">
-            <div class="card shadow mb-4">
-              <div class="card-body text-center">
-                <h1 class="display-4 text-primary">Welcome to Your Dashboard</h1>
-                <p class="lead">This is your personalized dashboard where you can manage your profile, settings, and other activities.</p>
-              </div>
-            </div>
-          </div>
+<!-- Begin Page Content -->
+<div class="container-fluid">
+  <!-- Welcome Message Section -->
+  <div class="row justify-content-center">
+    <div class="col-lg-6 col-md-8">
+      <div class="card shadow mb-4">
+        <div class="card-body text-center">
+          <h1 class="display-4 text-primary">Welcome to Your Dashboard!</h1>
+          <p class="lead">This is your personalized dashboard where you can manage your profile, settings, and other activities.</p>
         </div>
       </div>
-      <!-- /.container-fluid -->
     </div>
-    <!-- End of Content -->
+  </div>
+</div>
+<!-- /.container-fluid -->
+</div>
+<!-- End of Content -->
 
 
 
